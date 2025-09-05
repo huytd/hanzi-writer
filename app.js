cda13f5d33c7a71450fd369b7cee62d016c05576
Huy <kingbazoka@gmail.com>
2025-09-05 16:57:28 -0700

    One more...

--- app.js
+++ app.js
@@ -410,7 +410,7 @@ function initializeHanziWriter(character) {
             strokeAnimationSpeed: 1,
             delayBetweenStrokes: 300,
             strokeColor: '#2563eb',
-            drawingWidth: 8,
+            drawingWidth: 24,
             radicalColor: '#dc2626',
             highlightColor: '#fbbf24',
             outlineColor: '#d1d5db',
